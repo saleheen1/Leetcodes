d799329230e58693527d5b777734cc9da814140e
saleheen1 <smsaleheen3@gmail.com>
2024-10-02 21:15:03 +0600

    valid sudoku problem solved

---
+++ solved/valid_sudoku.py
@@ -0,0 +1,43 @@
+class Solution:
+    def isValidSudoku(self, board: list[list[str]]) -> bool:
+        seenRows = set()
+        seenColums = set()
+        seenBoxes = set()
+
+        for i in range(9):
+            for j in range(9):
+                char = board[i][j]
+                if char == ".":
+                    continue
+                singleRow = "char "+str(char) + " at row " + str(i)
+                singleColumn = "char "+ str(char) + " at column "+str(j)
+                boxNumber = str(int(i)/3) + str(int(j)/3)
+                singleBox = "char "+ str(char) + " at box "+ boxNumber
+
+                if(singleRow in seenRows or singleColumn in seenColums or singleBox in seenBoxes):
+                    return False
+                
+                seenRows.add(singleRow)
+                seenColums.add(singleColumn)
+                seenBoxes.add(singleBox)
+
+        return True
+
+
+
+
+
+s = Solution()
+board = [
+ ["1","2",".",".","3",".",".",".","."],
+ ["1",".",".","5",".",".",".",".","."],
+ [".","9","8",".",".",".",".",".","3"],
+ ["5",".",".",".","6",".",".",".","4"],
+ [".",".",".","8",".","3",".",".","5"],
+ ["7",".",".",".","2",".",".",".","6"],
+ [".",".",".",".",".",".","2",".","."],
+ [".",".",".","4","1","9",".",".","8"],
+ [".",".",".",".","8",".",".","7","9"]
+ ]
+
+s.isValidSudoku(board=board)
