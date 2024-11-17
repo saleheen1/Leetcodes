a18d9d05204ad9f6fd0a4e93f8c0237e0f6ed00a
saleheen1 <smsaleheen3@gmail.com>
2024-11-18 02:28:59 +0600

    min cost climb stairs - dp - bottom up tabulation

---
+++ solved/min_cost_climb_stairs_dp.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: list[int]) -> int:
+
+        # bottom up - tabulation
+        n = len(cost)
+        dp = [0] * (n+1)
+
+        for i in range(2,n+1):
+            dp[i] = min(dp[i-2] + cost[i-2] , dp[i-1]+cost[i-1])
+        
+        return dp[n]
