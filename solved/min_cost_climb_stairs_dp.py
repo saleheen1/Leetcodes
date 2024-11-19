58ee3fe1e8556e56d205d22d59072431347b6df8
saleheen1 <smsaleheen3@gmail.com>
2024-11-19 11:55:19 +0600

    house robber dp problem solved

--- solved/min_cost_climb_stairs_dp.py
+++ solved/min_cost_climb_stairs_dp.py
@@ -6,6 +6,9 @@ class Solution:
         dp = [0] * (n+1)
 
         for i in range(2,n+1):
-            dp[i] = min(dp[i-2] + cost[i-2] , dp[i-1]+cost[i-1])
+            dp[i] = min(dp[i-2] + cost[i-2] , dp[i-1] + cost[i-1])
         
         return dp[n]
+
+        
+        
