aa1d42e15aa78d338e79a6316c9d80ecc6762252
saleheen1 <smsaleheen3@gmail.com>
2024-12-25 23:08:30 +0600

    implement group anagrams solution with test cases

--- solved/test.py
+++ solved/test.py
@@ -1,23 +1,19 @@
 from collections import defaultdict
 
-
-class Solution:
+class Solution(object):
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
         res = defaultdict(list)
         
         for word in strs:
             count = [0] * 26
-            for letter in word:
-                count[ord(letter) - ord("a")] +=1
-
+            for v in word:
+                count[ord(v) - ord('a')] +=1
+            
             res[tuple(count)].append(word)
-
-        return list(res.values())
-
-        
+        return res.values()
 
 
+            
 
 s = Solution()
-
-print(s.groupAnagrams(strs = ["acct","tcca","tops","cat"]))
+print(s.groupAnagrams(strs=["act","pots","tops","cat","stop","hat"]))
